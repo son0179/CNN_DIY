f120882c7aad06cf973f7b436b6910384dc4a600
son0179 <starson2648@gmail.com>
2021-01-31 23:11:29 +0900

    아아아아아dkdk

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ Created on Sat Jan 30 22:33:55 2021
 
 import cupy as np
 
-class TwoLayerCNN:
+class ThrreeLayerCNN:
     
     def __init__(self):
         self.W = None
     def train(self , X , y ,learning_rate=1e-3, reg=1e-5, num_iters=100, batch_size=100):
 
         # ReLU류 활성화 함수를 위한 가중치 초기화
+        # Xavier initialzation 사용
         if self.W = None:
             W = np.random.randn(fan_in,fan_out)/np.sqrt(fan_in/2)
-        # Xavier initialzation 사용
-        N , =X.shape
+        
+        
+        
     def predict():
-    
+
+        
+
+
+class Conv1 :
+    def 
